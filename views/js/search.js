4cf38ebe3312722223087460fb308181364a8e83
eloicito333 <eloixxl2008@gmail.com>
2022-09-08 19:04:15 +0200

    trying to solve problem test 2

--- views/js/search.js
+++ views/js/search.js
@@ -11,7 +11,7 @@ const insertAfter = (newNode, existingNode) => {
 const searchOnIframe = (url) => {
     const hiddenFormElement = document.createElement('form');
     hiddenFormElement.setAttribute('id', 'hidden-form');
-    hiddenFormElement.setAttribute('action', 'http://www.beepbox.es/');
+    hiddenFormElement.setAttribute('action', '//www.beepbox.es/');
     hiddenFormElement.setAttribute('target', 'your-search');
     hiddenFormElement.setAttribute('method', 'POST');
 

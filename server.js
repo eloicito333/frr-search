a4cfcaee47881f051fa48ed72192a73cb4c0ce91
eloicito333 <eloixxl2008@gmail.com>
2022-09-11 20:35:59 +0200

    prepared to deploy on heroku witht deny https

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const Corrosion = require('corrosion');
 const dotenv = require('dotenv');
 
 dotenv.config()
@@ -12,8 +13,25 @@ dotenv.config()
     cert: fs.readFileSync(path.join(__dirname, '/ssl/server.cert')),
 }; */
 
+const denyHTTPS = (req, res, next) => {
+    if (req.secure && req.get('x-forwarded-proto') === 'https') {
+        return res.redirect(`http://${req.get('host')}${req.url}`);
+    }
+    next();
+}
 
+app.use(denyHTTPS);
 app.use(express.static(path.join(__dirname, '/views')));
+
+const proxy = new Corrosion({
+    codec: 'xor', // apply basic xor encryption to url parameters in an effort to evade filters. Optional.
+    prefix: '/get/', // specify the endpoint (prefix). Optional.
+    title: 'Gataway',
+    ssl: true
+});
+
+proxy.bundleScripts();
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/index.html'));
@@ -27,4 +45,14 @@ app.get('/feedback', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/feedback.html'));
 })
 
-app.listen(process.env.PORT || 8000);
+app.get(/\/get\/*/, (req, res) => {
+    return proxy.request(req, res);
+});
+
+app.post(/\/get\/*/, (req, res) => {
+    return proxy.request(req, res);
+});
+
+app.on('upgrade', (clientRequest, clientSocket, clientHead) => proxy.upgrade(clientRequest, clientSocket, clientHead));
+
+app.listen(process.env.PORT || 8080, () => console.log(`Server running at port: ${process.env.PORT}`));
